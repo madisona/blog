82ed9da07597ebe5ab112bab482202b5d23949c8
Aaron <aaron.l.madison@gmail.com>
2010-10-06 20:46:06 -0500

    tests for getting recent posts

--- blog/tests.py
+++ blog/tests.py
@@ -5,7 +5,10 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
+from mock import Mock, patch
+
 from django import test
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 from blog import models
@@ -36,4 +39,27 @@ class PostModelTests(test.TestCase):
         )
         self.assertEqual(post.body_html, "<p>This is the <strong>body</strong></p>")
 
+    @patch.object(models.Post, 'objects')
+    def should_get_recent_active_posts(self, query_mock):
+        query_mock.filter.return_value = [1]
+        posts = models.Post.get_recent_posts()
+
+        self.assertEqual(query_mock.method_calls, [('filter', (), {'active': True})])
+        self.assertEqual(posts, [1])
+
+    @patch.object(models.Post, 'objects')
+    def should_limit_recent_posts(self, query_mock):
+        query_mock.filter.return_value = [1, 2, 3]
+        posts = models.Post.get_recent_posts(limit=2)
+        self.assertEqual(posts, [1, 2])
+
+
+class Acceptance(test.TestCase):
+
+    def setUp(self):
+        self.client = test.Client()
+
+    def should_hit_index_page(self):
+        response = self.client.get(reverse("blog:index"))
+        self.assertEqual(response.status_code, 200)
 
